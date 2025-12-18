b9194f3b800696275a5d18e664b6c831324c0549
crackiii <nadeem.ahmad.na@outlook.com>
2025-12-19 00:44:01 +0100

    add a drop message on the canvas

--- packages/xcalidraw/appState.ts
+++ packages/xcalidraw/appState.ts
@@ -127,6 +127,7 @@ export const getDefaultAppState = (): Omit<
     searchMatches: null,
     lockedMultiSelections: {},
     activeLockedId: null,
+    dragDropFeedback: null,
   };
 };
 
@@ -252,6 +253,7 @@ const APP_STATE_STORAGE_CONF = (<
   searchMatches: { browser: false, export: false, server: false },
   lockedMultiSelections: { browser: true, export: true, server: true },
   activeLockedId: { browser: false, export: false, server: false },
+  dragDropFeedback: { browser: false, export: false, server: false },
 });
 
 const _clearAppStateForStorage = <
